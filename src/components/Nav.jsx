b114bd371ed94cc52ad80c921150be84fab62908
Killian <martykillian7@gmail.com>
2024-12-03 11:30:55 +0100

    finalizing the header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,12 @@ import { navLinks } from '../constants'
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-        <nav className='flex justify-between iten-center max-container'>
+    <header className='px-8 py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
             <a href="/">
-            <img src={headerLogo} alt="Logo" width={130} height={29} />
+                <img src={headerLogo} alt="Logo" width={130} height={29} />
             </a>
-            <ul className='flex-1 flex jsutify-center item-center gap-16 max-lg:hidden'>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
                 {navLinks.map((item) => (
                     <li key={item.label}>
                         <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
@@ -19,6 +19,9 @@ const Nav = () => {
                     </li>
                 ))}
             </ul>
+            <div className='hidden max-lg:block'>
+                <img src={hamburger} alt="Hamburger" width={25} height={25} />
+            </div>
         </nav>
     </header>
   )
