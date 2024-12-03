4266615141f44ebf349575eb952831a6ff79fcb0
Killian <martykillian7@gmail.com>
2024-12-03 11:24:14 +0100

    init and NavBar build

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    Footer,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe
+};
