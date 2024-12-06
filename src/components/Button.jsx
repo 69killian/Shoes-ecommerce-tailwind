297101dbadc23953f3af316b3bdfd227b224c8a7
Killian <martykillian7@gmail.com>
2024-12-06 14:34:03 +0100

    SpecialOffer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Button = ( {label}) => {
+const Button = ({ label, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className="relative rounded-[4px] inline-flex items-center justify-center h-10 px-9 text-base font-medium text-[#266131] bg-white border border-solid border-[#266131] transition-shadow duration-300 ease-in-out shadow-[4px_4px_0px_#266131] hover:shadow-none">
+    <button 
+      className={`relative rounded-[4px] inline-flex items-center justify-center h-10 px-9 text-base font-medium border 
+        ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'text-[#266131] bg-white border border-solid border-[#266131] transition-shadow duration-300 ease-in-out shadow-[4px_4px_0px_#266131] hover:shadow-none'}`}
+    >
       {label} 
     </button>
   );
